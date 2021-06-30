27304c47e09099deded379e188661c2765c1374a
booshja <jacob.andes@gmail.com>
2021-06-29 21:25:06 -0700

    launching construction page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 // dependencies
 import React from "react";
+import styled from "styled-components";
+// pages
+import Construction from "./Pages/Construction/Construction";
+// global styles
+import GlobalStyle from "./globalStyles";
+
+const StyledApp = styled.div`
+    min-height: 100vh;
+    background-color: rgb(230, 211, 169);
+    font-family: Century Gothic, CenturyGothic, AppleGothic, sans-serif;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
 
 function App() {
     return (
-        <div className="App">
-            {/*  */}
-            {/*  */}
-        </div>
+        <StyledApp className="App">
+            <GlobalStyle />
+            <Construction />
+        </StyledApp>
     );
 }
 

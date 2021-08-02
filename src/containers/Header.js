868d9f142ed92045e05442ec769ea7e70d2492dc
booshja <jacob.andes@gmail.com>
2021-08-02 14:24:55 -0700

    feat: added header component
    Added Header component
    
    References: PJ-63
    Closes: PJ-73

---
+++ src/containers/Header.js
@@ -0,0 +1,34 @@
+// dependencies
+import React from "react";
+import styled from "styled-components";
+// components
+import { Burger, MobileMenu } from "../components/index";
+
+const StyledHeader = styled.header`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 60px;
+    padding: 0 1rem;
+    border-bottom: 1px solid #0e6500;
+`;
+
+const StyledP = styled.p`
+    font-family: "Barlow Condensed", sans-serif;
+    font-size: 1.8rem;
+    font-weight: 700;
+    letter-spacing: 1px;
+    color: #0e6500;
+`;
+
+const Header = () => {
+    return (
+        <StyledHeader>
+            <StyledP>Painting Joy Mural Co.</StyledP>
+            <Burger />
+            <MobileMenu />
+        </StyledHeader>
+    );
+};
+
+export default Header;

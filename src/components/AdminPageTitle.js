67c7e6b6ca5eab89c8a7294cf2f12764301dd89f
booshja <jacob.andes@gmail.com>
2021-08-27 19:48:11 -0700

    feat: added AdminPageTitle
    Added AdminPageTitle component and tests
    
    References: PJ-121
    Closes: PJ-131

---
+++ src/components/AdminPageTitle.js
@@ -0,0 +1,16 @@
+// dependencies
+import React from "react";
+import styled from "styled-components";
+
+const StyledAdminPageTitle = styled.h1`
+    font-family: "Barlow Condensed", sans-serif;
+    font-size: 2.4rem;
+    letter-spacing: 1px;
+    padding: 1rem 1rem;
+`;
+
+const AdminPageTitle = ({ children }) => {
+    return <StyledAdminPageTitle>{children}</StyledAdminPageTitle>;
+};
+
+export default AdminPageTitle;
